1da03dceadbc0b8fb5874e4e763e04c4317d2fba
hsyndeniz <huseyin.deniz1@hotmail.com>
2024-11-25 08:52:25 +0300

    enhance Zustand store to manage characters and selected characters

--- store/index.ts
+++ store/index.ts
@@ -1,17 +1,26 @@
 import { create } from 'zustand';
+import { Character } from './api';
 
 interface SearchStore {
 	searchQuery: string;
 	currentPage: number;
+	characters: Character[];
+	selectedCharacters: Character[];
 	setSearchQuery: (query: string) => void;
 	setCurrentPage: (page: number) => void;
 	resetPagination: () => void;
+	setCharacters: (characters: Character[]) => void;
+	setSelectedCharacters: (characters: Character[]) => void;
 }
 
 export const useSearchStore = create<SearchStore>(set => ({
 	searchQuery: '',
 	currentPage: 1,
+	characters: [],
+	selectedCharacters: [],
 	setSearchQuery: query => set({ searchQuery: query, currentPage: 1 }), // Reset page on new search
 	setCurrentPage: page => set({ currentPage: page }),
 	resetPagination: () => set({ currentPage: 1 }),
+	setCharacters: characters => set({ characters }),
+	setSelectedCharacters: characters => set({ selectedCharacters: characters }),
 }));
